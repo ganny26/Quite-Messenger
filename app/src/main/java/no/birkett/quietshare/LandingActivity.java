b38c6fd02c42865c748664ba7f751f910837bc82
selvaganeshrajam@gmail.com <selvaganeshrajam@gmail.com>
2017-10-26 18:15:33 +0530

    landing activity updated

--- app/src/main/java/no/birkett/quietshare/LandingActivity.java
+++ app/src/main/java/no/birkett/quietshare/LandingActivity.java
@@ -1,5 +1,6 @@
 package no.birkett.quietshare;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -11,6 +12,11 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import org.quietmodem.Quiet.FrameTransmitter;
+import org.quietmodem.Quiet.FrameTransmitterConfig;
+import org.quietmodem.Quiet.ModemException;
+
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class LandingActivity extends AppCompatActivity implements View.OnClickListener{
@@ -23,6 +29,8 @@ public class LandingActivity extends AppCompatActivity implements View.OnClickLi
     private TextView balancetv ;
     private  LinearLayoutManager linearLayoutManager;
     private ArrayList<TransactionObject> mTransactionList = new ArrayList<TransactionObject>();
+    private FrameTransmitter transmitter;
+
 
 
     @Override
@@ -31,6 +39,7 @@ public class LandingActivity extends AppCompatActivity implements View.OnClickLi
         setContentView(R.layout.activity_landing);
         setUp();
         setUpTransactionAdapter();
+        handleDataFromIntent();
 
     }
 
@@ -52,11 +61,20 @@ public class LandingActivity extends AppCompatActivity implements View.OnClickLi
         balancetv = (TextView) findViewById(R.id.balance_info);
         amountToTransfer = (EditText) findViewById(R.id.amount);
         Button sendMoney = (Button) findViewById(R.id.send_money);
+        Button receiveMoney = (Button) findViewById(R.id.receive_money);
         balancetv.setText(transactionObject.getBalance());
         sendMoney.setOnClickListener(this);
+        receiveMoney.setOnClickListener(this);
 
     }
 
+
+    private void receiveMoney() {
+        startActivity(new Intent(this, ReceiveMoney.class));
+//        Intent intent = new Intent(LandingActivity.this,ReceiveMoney.class);
+//        LandingActivity.this.startActivity(intent);
+
+    }
 
     @Override
     public void onClick(View view) {
@@ -64,12 +82,59 @@ public class LandingActivity extends AppCompatActivity implements View.OnClickLi
             case R.id.send_money:
                 sendMoney(amountToTransfer.getText().toString());
                 break;
+            case R.id.receive_money:
+                receiveMoney();
+                break;
         }
     }
 
+//    private void setupTransmitter() {
+//        FrameTransmitterConfig transmitterConfig;
+//        try {
+//            transmitterConfig = new FrameTransmitterConfig(this,ProfilesHelper.getSoundOption(this));
+//            transmitter = new FrameTransmitter(transmitterConfig);
+//            Log.i(TAG,"Transmitted Data >>>>>"+transmitter.toString());
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        } catch (ModemException e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
+
+    private void handleDataFromIntent() {
+        Intent intent = getIntent();
+        String action = intent.getAction();
+        String type = intent.getType();
+        if (Intent.ACTION_SEND.equals(action) && type != null) {
+            if ("text/plain".equals(type)) {
+                String sharedText = intent.getStringExtra(Intent.EXTRA_TEXT);
+                amountToTransfer.setText(sharedText);
+            }
+        }
+    }
+
+
+
+    private void handleSendClick() {
+        if (transmitter == null) {
+           //setupTransmitter();
+        }
+        send();
+    }
+
+
+    private void send() {
+        String payload = amountToTransfer.getText().toString();
+        try {
+            transmitter.send(payload.getBytes());
+        } catch (IOException e) {
+
+        }
+    }
     private void sendMoney(String amt) {
         Float balance = Float.valueOf(transactionObject.getBalance()) - Float.valueOf(amt);
         Log.i(TAG,"Sending money"+balance);
+        //handleSendClick();
         transactionObject.setBalance(balance.toString());
         TransactionObject sObj = new TransactionObject(balance.toString(),"TR1231",amt,AppUtils.getTime());
         balancetv.setText(balance.toString());
