007ab2451f68aa83d5376897af4d571471f8a8c0
selvaganeshr <selvaganeshrajam@gmail.com>
2017-10-27 19:27:20 +0530

    qr receiver activity added

---
+++ app/src/main/java/no/birkett/quietshare/ReceiveQRAcrivity.java
@@ -0,0 +1,13 @@
+package no.birkett.quietshare;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReceiveQRAcrivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receive_qracrivity);
+    }
+}
